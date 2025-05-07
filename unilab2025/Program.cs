1d0900a13670acdbd7892abffba1f216b335a0e3
ryotaiijima <ring2510ring@gmail.com>
2025-05-07 22:44:19 +0900

    1

--- unilab2025/Program.cs
+++ unilab2025/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,14 +16,13 @@ namespace unilab2025
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Title());
         }
     }
